41312139ac084a13098b9f67026792a50b315b88
om salunke <omsaunke343@gmail.com>
2025-06-25 10:42:35 +0530

    google adsence script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,53 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Script from 'next/script'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Multi UI - Beautiful React Components',
-  description: 'A collection of beautifully designed, ready-to-use React components',
+  title: "Multi UI - Beautiful React Components",
+  description:
+    "A collection of beautifully designed, ready-to-use React components",
   icons: {
     icon: [
       {
-        url: '/favicon.svg',
-        type: 'image/svg+xml',
-      }
+        url: "/favicon.svg",
+        type: "image/svg+xml",
+      },
     ],
-    shortcut: ['/favicon.svg'],
+    shortcut: ["/favicon.svg"],
     apple: [
       {
-        url: '/favicon.svg',
-        type: 'image/svg+xml',
-      }
+        url: "/favicon.svg",
+        type: "image/svg+xml",
+      },
     ],
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7125097812565053"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>
         <Navbar />
-        <main className="">
-          {children}
-        </main>
+        <main className="">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
